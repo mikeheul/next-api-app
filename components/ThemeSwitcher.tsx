ef5300cce866e26a5e91d14f7de06ca41a33e822
Micka <mickael.murmann@gmail.com>
2024-05-07 17:56:02 +0200

    Position of ThemeSwitcher component

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -20,7 +20,7 @@ export const ThemeSwitcher = () => {
     return (
         <button
             style={{ zIndex: 9999 }}
-            className={`w-fit fixed right-5 top-2 p-2 rounded-md hover:scale-110 active:scale-100 duration-200 bg-slate-200 dark:bg-[#212933]`}
+            className={`w-fit fixed right-6 top-4 p-2 rounded-md hover:scale-110 active:scale-100 duration-200 bg-slate-200 dark:bg-[#212933]`}
             onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
         >
             {theme === "light" ? <MoonIcon /> : <SunIcon />}
