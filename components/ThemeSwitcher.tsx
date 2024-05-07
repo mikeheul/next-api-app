eefc5b7df2426c2ad7c85ad013ba4dec179dadcf
Micka <mickael.murmann@gmail.com>
2024-05-07 17:54:46 +0200

    z-index of ThemeSwitcher component

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -19,8 +19,9 @@ export const ThemeSwitcher = () => {
 
     return (
         <button
-        className={`w-fit absolute right-5 top-2 p-2 rounded-md hover:scale-110 active:scale-100 duration-200 bg-slate-200 dark:bg-[#212933]`}
-        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            style={{ zIndex: 9999 }}
+            className={`w-fit fixed right-5 top-2 p-2 rounded-md hover:scale-110 active:scale-100 duration-200 bg-slate-200 dark:bg-[#212933]`}
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
         >
             {theme === "light" ? <MoonIcon /> : <SunIcon />}
         </button>
